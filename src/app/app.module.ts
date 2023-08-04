047cb3500edbbbeb1bb487c15a2ae8d52b8740a3
unknown <walid.el.biir@gmail.com>
2023-08-04 12:26:19 +0100

    building the navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { ProfileComponent } from './layouts/profile/profile.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { MainComponent } from './layouts/main/main.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    ProfileComponent,
+    FooterComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
