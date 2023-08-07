3bb7819399050447a5504d0b668563820bb0da92
unknown <walid.el.biir@gmail.com>
2023-08-07 10:50:57 +0100

    building the topbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { ProfileComponent } from './layouts/profile/profile.component';
-import { FooterComponent } from './layouts/footer/footer.component';
 import { MainComponent } from './layouts/main/main.component';
 import { ButtonModule } from 'primeng/button';
+import { TopBarComponent } from './layouts/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProfileComponent,
-    FooterComponent,
     MainComponent,
+    TopBarComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/layouts/top-bar/top-bar.component.spec.ts
+++ src/app/layouts/top-bar/top-bar.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FooterComponent } from './footer.component';
+import { TopBarComponent } from './top-bar.component';
 
-describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
+describe('TopBarComponent', () => {
+  let component: TopBarComponent;
+  let fixture: ComponentFixture<TopBarComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FooterComponent]
+      declarations: [TopBarComponent]
     });
-    fixture = TestBed.createComponent(FooterComponent);
+    fixture = TestBed.createComponent(TopBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
